9788e1a49ba5f23d3cb89798a6dc5afa7b8bb7ac
UTKARSHPDUBEY <utkarshpdubey@gmail.com>
2026-07-16 23:41:14 +0530

    Implement Search API

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -21,14 +21,9 @@ connectToDatabase().then(() => {
 
 app.use(express.json());
 
-const giftRoutes= require("./routes/giftRoutes")
-app.use("/api/gifts",giftRoutes)
 // Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-//{{insert code here}}
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+const giftRoutes = require("./routes/giftRoutes");
+const searchRoutes = require("./routes/searchRoutes");
 
 
 const pinoHttp = require('pino-http');
@@ -36,12 +31,8 @@ const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
 
-// Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
-
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use("/api/gifts", giftRoutes);
+app.use("/api/search", searchRoutes);
 
 
 // Global Error Handler
