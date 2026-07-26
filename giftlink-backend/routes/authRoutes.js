3e8f78cf9c2451e4c4d8ebdfe090c3264a8d60d4
UTKARSHPDUBEY <utkarshpdubey@gmail.com>
2026-07-26 23:07:49 +0530

    Develop Back-end Integration for Login

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -73,4 +73,64 @@ router.post('/register', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    try {
+
+        // Task 1
+        const db = await connectToDatabase();
+
+        // Task 2
+        const users = db.collection("users");
+
+        // Task 3
+        const user = await users.findOne({
+            email: req.body.email
+        });
+
+        // Task 7
+        if (!user) {
+            return res.status(400).json({
+                error: "User not found"
+            });
+        }
+
+        // Task 4
+        const passwordCompare = await bcryptjs.compare(
+            req.body.password,
+            user.password
+        );
+
+        if (!passwordCompare) {
+            return res.status(400).json({
+                error: "Invalid credentials"
+            });
+        }
+
+        // Task 5
+        const userName = user.firstName;
+        const userEmail = user.email;
+
+        // Task 6
+        const payload = {
+            user: {
+                id: user._id
+            }
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+
+        res.json({
+            authtoken,
+            userName,
+            userEmail
+        });
+
+    } catch (e) {
+
+        logger.error(e);
+
+        return res.status(500).send("Internal server error");
+
+    }
+});
 module.exports = router;
